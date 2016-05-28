42d8f535c25f41613e1626cf20fa1a9ef9be11d1
jin <jinmin.hu@altran.com>
2016-05-28 14:25:08 +0200

    rename package again

--- android/app/src/main/java/net/manatree/chat/MyFirebaseMessagingService.java
+++ android/app/src/main/java/net/manatree/chat/MyFirebaseMessagingService.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package net.manatree.chat.friendlychat;
+ */package net.manatree.chat;
 
 import android.util.Log;
 
--- android/app/src/main/java/net/manatree/chat/SignInActivity.java
+++ android/app/src/main/java/net/manatree/chat/SignInActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.manatree.chat.friendlychat;
+package net.manatree.chat;
 
 import android.content.Intent;
 import android.os.Bundle;
